09f1a97cba32bdd6128a76b746735c85b63906fb
kimlindner <klindner@mail.uni-mannheim.de>
2021-10-23 18:29:18 +0200

    Update Entity_Clean with language detection.py
    run for more entites & save removed row IDs in dictionary

--- notebooks/Entity_Clean with language detection.py
+++ notebooks/Entity_Clean with language detection.py
@@ -9,7 +9,6 @@ fasttext.FastText.eprint = lambda x: None # avoid Warning : `load_model` does no
 
 
 path_parent = os.path.dirname(os.getcwd())
-data_path = os.path.join(path_parent, 'src/data')
 
 
 def remove_irrelevant_tlds():
@@ -17,7 +16,7 @@ def remove_irrelevant_tlds():
     moves all files with valid tlds to a new path called "cleaned"
     :return:
     """
-    files = [file for file in os.listdir(top_100_path) if file.endswith('.json.gz')]
+    files = [file for file in os.listdir(data_path) if file.endswith('.json.gz')]
 
     valid_tld = ['.com', '.net', '.org', '.uk']
     valid_files = []
@@ -35,7 +34,7 @@ def remove_irrelevant_tlds():
             if file_valid == 'true':
                 valid_files.append(file)
                 # copy only files with valid tlds to cleaned path
-                shutil.copy(os.path.join(top_100_path, '{}'.format(file)), cleaned_top_100_path, follow_symlinks=True)
+                shutil.copy(os.path.join(data_path, '{}'.format(file)), cleaned_data_path, follow_symlinks=True)
 
             count += 1
             bar.update(count)
@@ -52,71 +51,97 @@ def remove_with_fasttext():
     model = fasttext.load_model(pretrained_fasttext_path)
 
     # list all top 100 product files
-    files = [file for file in os.listdir(cleaned_top_100_path) if file.endswith('.json.gz')]
+    files = [file for file in os.listdir(cleaned_data_path) if file.endswith('.json.gz')]
+
+    removed_rows_dict = {}
 
     count_files = 0
 
     for file in files:
         print(file)
-        df = pd.read_json(os.path.join(cleaned_top_100_path, '{}'.format(file)), compression='gzip', lines=True)
+        df = pd.read_json(os.path.join(cleaned_data_path, '{}'.format(file)), compression='gzip', lines=True)
         if df.shape[0] > 20:
             df['concat'] = ''
 
             for j in range(df.shape[1]):  # iterate over columns
                 df['concat'] = df['concat'] + df.iloc[:,j].astype('str')
 
-            #iterrate over rows and save row_ids of english products
+            # iterrate over rows and save row_ids of english products
             english_products = []
+            non_english_products = []
             count = 0
             with progressbar.ProgressBar(max_value=df.shape[0]) as bar:
                 for i in range(df.shape[0]):  # iterate over rows
-                    row_id = df['row_id'][i]
+                    row_id = int(df['row_id'][i])
                     cell = df['concat'][i]
                     cell_pred = model.predict([cell])[0]
                     if cell_pred == [['__label__en']]:
                         english_products.append(row_id)
+                    else:
+                        non_english_products.append(row_id)
                     count += 1
                     bar.update(count)
 
             # drop concatendated row again
             df = df.drop('concat', axis=1)
 
+            # write removed row_ids to dictionary
+            removed_rows_dict[file] = non_english_products
+
             # design new dataframe with english products only
             df_cleaned = df[df['row_id'].isin(english_products)]
 
-
             # write to gzip compressed json file
             if df_cleaned.shape[0] > 20:
-                df_cleaned.to_json(os.path.join(cleaned_top_100_path, '{}'.format(file)), compression='gzip', orient='records', lines=True)
+                df_cleaned.to_json(os.path.join(cleaned_data_path, '{}'.format(file)), compression='gzip', orient='records', lines=True)
             else:
-                os.remove(os.path.join(cleaned_top_100_path, '{}'.format(file)))
+                os.remove(os.path.join(cleaned_data_path, '{}'.format(file)))
 
 
         else:
             # if df does not contain more than 20 entries delete it from cleaned file path
-            os.remove(os.path.join(cleaned_top_100_path, '{}'.format(file)))
+            os.remove(os.path.join(cleaned_data_path, '{}'.format(file)))
 
         count_files += 1
         print('{} out of {} files done'.format(count_files, len(files)))
 
+    # save dictionary with removed row_ids
+    with open(os.path.join(cleaned_data_path, 'removed_rows_dict.json'), 'w', encoding='utf-8') as f:
+        json.dump(removed_rows_dict, f)
+
+    print('removed_rows_dict saved')
+
 
 # run functions
-# entities = ['product_top100', 'product_min3', 'localbusiness_top100', 'localbusiness_min3']
-entities = ['product_min3', 'localbusiness_top100', 'localbusiness_min3']
+entities = ['product_top100', 'product_minimum3', 'LocalBusiness_top100', 'LocalBusiness_minimum3', 'LocalBusiness_rest',
+            'Restaurant_top100', 'Restaurant_minimum3', 'Restaurant_rest', 'Hotel_top100', 'Hotel_minimum3', 'Hotel_rest']
+
 
 for entity in entities:
     if entity == 'product_top100':
-        top_100_path = os.path.join(data_path, 'product/product_top100')
-        cleaned_top_100_path = os.path.join(top_100_path, 'cleaned')
-    elif entity == 'product_min3':
-        top_100_path = os.path.join(data_path, 'product/product_minimum3')
-        cleaned_top_100_path = os.path.join(top_100_path, 'cleaned')
-    elif entity == 'localbusiness_top100':
-        top_100_path = os.path.join(data_path, 'LocalBusiness/LocalBusiness_top100')
-        cleaned_top_100_path = os.path.join(top_100_path, 'cleaned')
-    elif entity == 'localbusiness_min3':
-        top_100_path = os.path.join(data_path, 'LocalBusiness/LocalBusiness_minimum3')
-        cleaned_top_100_path = os.path.join(top_100_path, 'cleaned')
+        data_path = os.path.join(path_parent, 'src/data/product/product_top100')
+    elif entity == 'product_minimum3':
+        data_path = os.path.join(path_parent, 'src/data/product/product_minimum3')
+    elif entity == 'LocalBusiness_top100':
+        data_path = os.path.join(path_parent, 'src/data/LocalBusiness/LocalBusiness_top100')
+    elif entity == 'LocalBusiness_minimum3':
+        data_path = os.path.join(path_parent, 'src/data/LocalBusiness/LocalBusiness_minimum3')
+    elif entity == 'LocalBusiness_rest':
+        data_path = os.path.join(path_parent, 'src/data/LocalBusiness/LocalBusiness_rest')
+    elif entity == 'Restaurant_top100':
+        data_path = os.path.join(path_parent, 'src/data/Restaurant/Restaurant_top100')
+    elif entity == 'Restaurant_minimum3':
+        data_path = os.path.join(path_parent, 'src/data/Restaurant/Restaurant_minimum3')
+    elif entity == 'Restaurant_rest':
+        data_path = os.path.join(path_parent, 'src/data/Restaurant/Restaurant_rest')
+    elif entity == 'Hotel_top100':
+        data_path = os.path.join(path_parent, 'src/data/Restaurant/Hotel_top100')
+    elif entity == 'Hotel_minimum3':
+        data_path = os.path.join(path_parent, 'src/data/Restaurant/Hotel_minimum3')
+    elif entity == 'Hotel_rest':
+        data_path = os.path.join(path_parent, 'src/data/Restaurant/Hotel_rest')
+
+    cleaned_data_path = os.path.join(data_path, 'cleaned')
 
     print('running {} path'.format(entity))
 
