2d853ea7e5b6008d44a7a6c69a8a5afe2a11edab
kimlindner <klindner@mail.uni-mannheim.de>
2021-10-31 13:49:00 +0100

    Update Entity_Product_Clean cluster tables & Allocation.py
    updated threading

--- notebooks/Entity_Product_Clean cluster tables & Allocation.py
+++ notebooks/Entity_Product_Clean cluster tables & Allocation.py
@@ -316,6 +316,7 @@ def thread_function(name):
     logging.info("Thread %s: finishing", name)
 
 if __name__ == "__main__":
+    os.environ['NUMEXPR_MAX_THREADS'] = '24'
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
