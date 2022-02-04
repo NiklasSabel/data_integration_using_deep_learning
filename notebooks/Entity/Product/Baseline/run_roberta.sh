8834a2eebe7515c8b65415e0b5bafe32c650aa2a
Niklas Sabel <niklas.sabel@comm-uni.com>
2022-02-04 17:44:55 +0100

    Baseline TinyBert

--- notebooks/Entity/Product/Baseline/run_roberta.sh
+++ notebooks/Entity/Product/Baseline/run_roberta.sh
@@ -11,7 +11,7 @@ python transformer_roberta_debugged.py \
     --do_train \
     --do_predict \
     --train_file /pfs/work7/workspace/scratch/ma_nsabel-  teamprojekt/data_integration_using_deep_learning/src/data/product/train_test_split/output_unfiltered_tables/large/after_manual_checking/baselines/df_train \
-    --val_file /pfs/work7/workspace/scratch/ma_nsabel-  teamprojekt/data_integration_using_deep_learning/src/data/product/train_test_split/output_unfiltered_tables/large/after_manual_checking/baselines/df_val \
+    --validation_file /pfs/work7/workspace/scratch/ma_nsabel-  teamprojekt/data_integration_using_deep_learning/src/data/product/train_test_split/output_unfiltered_tables/large/after_manual_checking/baselines/df_val \
 	--test_file /pfs/work7/workspace/scratch/ma_nsabel-  teamprojekt/data_integration_using_deep_learning/src/data/product/train_test_split/output_unfiltered_tables/large/after_manual_checking/baselines/df_test \
 	--tokenizer_name="mrm8488/bert-tiny-finetuned-squadv2" \
     --output_dir /pfs/work7/workspace/scratch/ma_nsabel-teamprojekt/data_integration_using_deep_learning/notebooks/Entity/Product/Baseline/output_tinybert/ \
