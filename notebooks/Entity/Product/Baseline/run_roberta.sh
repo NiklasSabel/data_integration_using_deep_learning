4901e21791b243b1910b3de12da3cbaaf24f8cd7
Niklas Sabel <niklas.sabel@comm-uni.com>
2022-02-13 12:45:05 +0100

    Structuring

--- notebooks/Entity/Product/Baseline/run_roberta.sh
+++ notebooks/Entity/Product/Baseline/run_roberta.sh
@@ -10,15 +10,17 @@ BATCH=$2
 python transformer_roberta_debugged.py \
     --do_train \
     --do_predict \
-    --num_train_epochs=50\
+    --num_train_epochs=100\
     --train_file /pfs/work7/workspace/scratch/ma_nsabel-teamprojekt/data_integration_using_deep_learning/src/data/product/train_test_split/output_unfiltered_tables/large/after_manual_checking/baselines/df_train.csv \
     --validation_file /pfs/work7/workspace/scratch/ma_nsabel-teamprojekt/data_integration_using_deep_learning/src/data/product/train_test_split/output_unfiltered_tables/large/after_manual_checking/baselines/df_val.csv \
     --test_file /pfs/work7/workspace/scratch/ma_nsabel-teamprojekt/data_integration_using_deep_learning/src/data/product/train_test_split/output_unfiltered_tables/large/after_manual_checking/baselines/df_test.csv \
-    --tokenizer_name="bert-base-uncased" \
+    --tokenizer_name="huawei-noah/TinyBERT_General_4L_312D" \
     --gradient_accumulation_steps=2 \
-    --save_steps=10 \
     --logging_steps=3 \
     --warmup_steps=10 \
+    --load_best_model_at_end=True\
+    --metric_for_best_model="eval_accuracy"\
     --evaluation_strategy="epoch" \
+    --save_strategy="epoch" \
     --output_dir /pfs/work7/workspace/scratch/ma_nsabel-teamprojekt/data_integration_using_deep_learning/notebooks/Entity/Product/Baseline/output_tinybert/ \
     --model_name_or_path="huawei-noah/TinyBERT_General_4L_312D" \
